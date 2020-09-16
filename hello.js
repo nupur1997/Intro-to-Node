ce92f8d8b6e7757004c8cfa001f2379fd10d456e
Nupur Jain <nupurjaincse@gmail.com>
2020-09-17 04:30:24 +0800

    Intial commit

---
+++ hello.js
@@ -0,0 +1,31 @@
+
+var http = require('http');
+var fs = require('fs');
+var url  = require('url');
+
+http.createServer(function(req, res){
+
+var q = url.parse(req.url, true);
+
+var filename = "." + q.pathname;
+
+if(filename == './'){filename = './index';}
+
+filename = filename + ".html";
+
+	fs.readFile(filename, function(err, data) {
+		if(err) {
+			res.writeHead(400, {'Content-Type': 'text/html'});
+			return res.end("404 NOT FOUND");
+		}
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(data);
+	//console.log("....Incoming request" + req.url);
+	return res.end();
+});
+	//res.end('Hello World! My name is Nupur');
+}).listen(8080);
+
+console.log("Server listner onn port 8080");
+
